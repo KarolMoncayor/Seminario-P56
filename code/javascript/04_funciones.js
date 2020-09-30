d34e172fab3cf03c51ea904bbe6687b91556f893
Karol Moncayo <kmoncayor@est.ups.edu.ec>
2020-09-30 02:04:33 -0500

    Estructuras respetitivas.

--- code/javascript/04_funciones.js
+++ code/javascript/04_funciones.js
@@ -1,9 +1,17 @@
 var nombreApellido = "Karol Moncayo"
 var edad = 37
 
+//funcion tipo declarativa 
 function imprimirEdad(nombre, edad){
     console.log(`${nombre} tiene la edad de ${edad}.`)
 }
-
+function imprimirEdad2(nombre, edad){
+    console.log(`${nombre} tiene la edad de ${edad}.`)
+}
+function imprimirEdad3(nombre, edad){
+    console.log(`${nombre} tiene la edad de ${edad}.`)
+}
 imprimirEdad( nombreApellido, edad)
-imprimirEdad( "Ruth Alvarado", 1)
+imprimirEdad("Ruth Alvarado", 1)
+imprimirEdad2 (nombreApellido, edad)
+imprimirEdad3 ("Jose Rosales", 17)
