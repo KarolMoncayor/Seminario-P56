ad96635559eb916830308747ff48ccc9a91223c9
Karol Moncayo <kmoncayor@est.ups.edu.ec>
2020-09-30 19:46:16 -0500

    Servidor de Node.js implementadas las solicitudes y respuestas

---
+++ code/node-fundamentals/01_server/server.js
@@ -0,0 +1,26 @@
+const express = require ('express')
+const bodyParser = require('body-parser')
+
+const router = express.Router()
+
+var app = express()
+app.use(bodyParser.json() )
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(router)
+
+router.get('/carrera', function(req, res) {
+     res.send('Listas de carerra de la UPS')
+})
+router.post('/carrera', function(req,res){
+
+console.log (req.body)
+console.log (req.query)
+
+
+res.status(200).send ({ tipo_error:0 , mensaje_error:'' ,mensaje_exito: 'Añadido Exitosamente'} )
+ 
+})
+app.use('/', express.static('public'))
+
+app.listen(5000)
+console.log('La aplicacion esta escuchando en http://localhost:5000')
